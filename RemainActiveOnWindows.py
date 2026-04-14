192bf51949116772e6464fbc3d7f9332a5c082f5
Hrishikesh Yadav <87562326+hrish11@users.noreply.github.com>
2026-04-14 14:16:44 +0530

    Update RemainActiveOnWindows.py

--- RemainActiveOnWindows.py
+++ RemainActiveOnWindows.py
@@ -1,12 +1,28 @@
 import pyautogui
 import time
+import keyboard
+import sys
 
 pyautogui.FAILSAFE = False
+    
+try:
+    while True:
+        if keyboard.is_pressed('esc'):
+            print("ESC pressed, exiting...")
+            sys.exit() 
+        time.sleep(15)
 
-while True:
-    time.sleep(15)
-    for i in range(0 ,100):
-        pyautogui.moveTo(0, i * 5)
+        for i in range(0, 100):
+            if keyboard.is_pressed('esc'):
+                print("ESC pressed, exiting...")
+                sys.exit()   
+            pyautogui.moveTo(0, i * 5)
 
-    for i in range(1, 3):
-        pyautogui.press('shift')
+        for i in range(0, 3):
+            if keyboard.is_pressed('esc'):
+                print("ESC pressed, exiting...")
+                sys.exit()  
+            pyautogui.press('shift')
+
+except Exception as e:
+    print(f"An error occurred: {e}")
